adb4695c4564b4c9736439e05dc56d1cefd44e9e
trewpog <arditazo3@gmail.com>
2019-05-31 22:42:25 +0200

    Initial commit

---
+++ src/main/java/com/apache/pool/XmlParser.java
@@ -0,0 +1,29 @@
+package com.apache.pool;
+
+public class XmlParser<E, T> implements Parser<E, T> {
+    private Exception exception;
+
+    private String test;
+
+    @Override
+    public void parse(E elementToBeParsed, T result) throws Exception {
+        try {
+            System.out.println("[" + Thread.currentThread().getName()+ "]: Parser Instance:" + this);
+            // Do some real parsing stuff.
+
+            test = "test";
+        } catch(Exception e) {
+            this.exception = e;
+            e.printStackTrace(System.err);
+            throw e;
+        }
+    }
+    @Override
+    public boolean isValid() {
+        return this.exception == null;
+    }
+    @Override
+    public void reset() {
+        this.exception = null;
+    }
+}
